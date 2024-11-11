d7cc1f87e5e36eace7f2cad359949d1af4df9eb8
ajasnin <arnaud.jasnin@gmail.com>
2024-11-11 14:18:46 +0100

    create another tableSQL to testing with django-admin

--- default/settings.py
+++ default/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'app',
+    'cars',
 ]
 
 MIDDLEWARE = [
@@ -72,12 +73,23 @@ WSGI_APPLICATION = 'default.wsgi.application'
 
 
 # Database
-# https://docs.djangoproject.com/en/5.1/ref/settings/#databases
+# # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'ft_transcendance',
+        'USER': 'yoda',
+        'PASSWORD': 'yoda-pwd',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
