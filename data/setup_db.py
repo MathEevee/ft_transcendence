2ff5275069026b96c2ae520345afc951417e5bbc
ajasnin <arnaud.jasnin@gmail.com>
2024-11-18 22:24:40 +0100

    create dir data/ and scipt to init_data "data/setup_db.py"

---
+++ data/setup_db.py
@@ -0,0 +1,61 @@
+import os
+import django
+
+# init Django Env
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "ft_transcendence.settings")
+django.setup()
+
+from django.core.management import call_command
+from django.db import IntegrityError
+from accounts.models import CustomUser
+from scores.models import PlayerStats
+
+def setup_database():
+    # 1 : execute makemigrations and migrate
+    print("Applying migrations...")
+    call_command("makemigrations", interactive=False)
+    call_command("migrate", interactive=False)
+
+    # 2 : create objects for table 'CustomUser'
+    print("Creating users...")
+    try:
+        CustomUser.objects.create_superuser(
+            username="ajas",
+            email="ajas@example.com",
+            password="SuperPower",
+        )
+        CustomUser.objects.create_user(
+            username="yoda",
+            email="yoda@example.com",
+            password="TheForce",
+        )
+        CustomUser.objects.create_user(
+            username="luke",
+            email="luke@example.com",
+            password="StarKiller",
+        )
+    except IntegrityError:
+        print("Users already exist, skipping user creation.")
+
+    # 3 : create objects for table 'PlayerStats'
+    print("Creating scores...")
+    try:
+        PlayerStats.objects.create(
+            username="yoda",
+            games_play=10,
+            games_win=7,
+            total_score=150
+        )
+        PlayerStats.objects.create(
+            username="luke",
+            games_play=12,
+            games_win=9,
+            total_score=200
+        )
+    except IntegrityError:
+        print("Scores already exist, skipping score creation.")
+
+    print("Database setup complete!")
+
+if __name__ == "__main__":
+    setup_database()
