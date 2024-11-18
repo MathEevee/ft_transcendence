2fac3154588c4d4fe1e33eec1f95d156f76a6333
ajasnin <arnaud.jasnin@gmail.com>
2024-11-18 23:12:37 +0100

    add "create_database" into data/setup_db.py

--- data/setup_db.py
+++ data/setup_db.py
@@ -1,8 +1,9 @@
 import os
+import subprocess
 import django
 
 # init Django Env
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "ft_transcendence.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "default.settings")
 django.setup()
 
 from django.core.management import call_command
@@ -10,7 +11,30 @@ from django.db import IntegrityError
 from accounts.models import CustomUser
 from scores.models import PlayerStats
 
+# create database if not exist
+def create_database(db_name, db_user):
+    try:
+        # check if database exist
+        subprocess.run(
+            ["psql", "-U", db_user, "-c", f"SELECT 1 FROM pg_database WHERE datname='{db_name}';"],
+            check=True,
+            text=True,
+        )
+        print(f"Database '{db_name}' already exists.")
+    except subprocess.CalledProcessError:
+        # create database
+        print(f"Creating database '{db_name}'...")
+        subprocess.run(
+            ["psql", "-U", db_user, "-c", f"CREATE DATABASE {db_name};"],
+            check=True,
+            text=True,
+        )
+        print(f"Database '{db_name}' created successfully.")
+
 def setup_database():
+    # 0 : create database if not exist
+    create_database("transcendance_db", "postgres")
+
     # 1 : execute makemigrations and migrate
     print("Applying migrations...")
     call_command("makemigrations", interactive=False)
