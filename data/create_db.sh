cb68ef9363d0eff68122a765b05d7a7148174c45
ajasnin <arnaud.jasnin@gmail.com>
2024-11-20 00:42:22 +0100

    data/create_db.sh: add permissions for public schema

--- data/create_db.sh
+++ data/create_db.sh
@@ -34,4 +34,14 @@ else
 
     # Assigner les privilèges
     sudo -u postgres psql -c "GRANT ALL PRIVILEGES ON DATABASE $DB_NAME TO $DB_USER;"
+
+    # permissions for public schema
+    echo "Configuring permissions for schema 'public'..."
+    sudo -u postgres psql -d $DB_NAME -c "ALTER SCHEMA public OWNER TO $DB_USER;"
+    sudo -u postgres psql -d $DB_NAME -c "GRANT ALL PRIVILEGES ON SCHEMA public TO $DB_USER;"
+    sudo -u postgres psql -d $DB_NAME -c "GRANT ALL PRIVILEGES ON ALL TABLES IN SCHEMA public TO $DB_USER;"
+    sudo -u postgres psql -d $DB_NAME -c "GRANT ALL PRIVILEGES ON ALL SEQUENCES IN SCHEMA public TO $DB_USER;"
+    sudo -u postgres psql -d $DB_NAME -c "GRANT ALL PRIVILEGES ON ALL FUNCTIONS IN SCHEMA public TO $DB_USER;"
+
+    echo "Database '$DB_NAME' and user '$DB_USER' successfully created and configured."
 fi
