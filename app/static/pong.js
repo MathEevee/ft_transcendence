641b622382ee240330d1183d457e5ead7fec3b60
PageBlanche <axeldubois@orange.fr>
2024-11-19 17:05:29 +0100

    fin du pong en vs local avec un bug à revoir

--- app/static/pong.js
+++ app/static/pong.js
@@ -17,6 +17,26 @@ let score2 = 0;
 let speed = 7;
 
 let start = 0;
+let option = 0;
+
+//options
+const colorpalette = ["#FFFFFF", "#000000", "#FF0000", "#00FF00", "#0000FF", "#FFFF00", "#FF00FF", "#00FFFF"];
+let colorindex = 0;
+
+colorset =
+{
+	fontcolor: colorpalette[0],
+	backgroundcolor: colorpalette[1],
+	ballcolor: colorpalette[0],
+	player1color: colorpalette[0],
+	player2color: colorpalette[0],
+	netcolor: colorpalette[0],
+	scorecolor: colorpalette[0],
+}
+
+const functionstochangeoption = [changeFontColor, changeBackgroundColor, changeBallColor, changePlayer1Color, changePlayer2Color, changeNetColor, changeScoreColor];
+let optionindex = 0;
+let keyoptionpressed = 0;
 
 
 /* Dessin */
@@ -33,32 +53,136 @@ function drawCircle(x, y, radius, color) {
 	context.closePath();
 	context.fill();
 }
-  
+
 function drawNet() {
 	for (let i = 0; i < canvas.height; i += 15) {
-		drawRect(canvas.width / 2 - 1, i, 2, 10, "#FFF");
+		drawRect(canvas.width / 2 - 1, i, 2, 10, colorset.netcolor);
 	}
 }
 
 function drawScore() {
-	context.fillStyle = "#FFF";
+	context.fillStyle = colorset.scorecolor;
 	context.font = "35px Arial";
 	context.fillText(score1, canvas.width / 4, 50);
 	context.fillText(score2, 3 * canvas.width / 4, 50);
 }
-  
+
 function draw() {
-	drawRect(0, 0, canvas.width, canvas.height, "#000"); // Fond
+	drawRect(0, 0, canvas.width, canvas.height, colorset.backgroundcolor); // Fond
 	drawNet();
-	drawRect(player1.x, player1.y, player1.width, player1.height, "#FFF"); // Raquette joueur 1
-	drawRect(player2.x, player2.y, player2.width, player2.height, "#FFF"); // Raquette joueur 2
-	drawCircle(ball.x, ball.y, ball.radius, "#FFF"); // Balle
+	drawRect(player1.x, player1.y, player1.width, player1.height, colorset.player1color); // Raquette joueur 1
+	drawRect(player2.x, player2.y, player2.width, player2.height, colorset.player2color); // Raquette joueur 2
+	drawCircle(ball.x, ball.y, ball.radius, colorset.ballcolor); // Balle
 	drawScore();
 }
 
-/* Mouvement des raquettes */
+function changeColor()
+{
+	if (optionindex === 0)
+		colorset.fontcolor = colorpalette[colorindex];
+	else if (optionindex === 1)
+		colorset.backgroundcolor = colorpalette[colorindex];
+	else if (optionindex === 2)
+		colorset.ballcolor = colorpalette[colorindex];
+	else if (optionindex === 3)
+		colorset.player1color = colorpalette[colorindex];
+	else if (optionindex === 4)
+		colorset.player2color = colorpalette[colorindex];
+	else if (optionindex === 5)
+		colorset.netcolor = colorpalette[colorindex];
+	else if (optionindex === 6)
+		colorset.scorecolor = colorpalette[colorindex];
+}
 
-document.addEventListener("keydown", (event) =>
+function drawoptioninstruction()
+{
+	context.fillStyle = colorset.fontcolor;
+	context.font = "20px Arial";
+	context.fillText("Press Enter to change option.", 20, canvas.height - 20);
+	context.fillText("Press Left or Right to change color.", 20, canvas.height - 40);
+}
+
+function drawcurrencolorpalette(x, y)
+{
+	context.fillStyle = colorset.fontcolor;
+	context.font = "80px Arial";
+	context.fillText("<", x, y);
+	context.font = "50px Arial";
+	context.fillText(colorpalette[colorindex], x + 55, y - 10);
+	context.font = "80px Arial";
+	context.fillText(">", x + 270, y);
+}
+
+function changeFontColor(colorname)
+{
+	drawoptioninstruction();
+	context.fillStyle = colorset.fontcolor;
+	context.font = "50px Arial";
+	context.fillText("FONT COLOR:", canvas.width / 2 - 150, 100);
+	drawcurrencolorpalette(200, 200);
+}
+
+function changeBackgroundColor(colorname)
+{
+	drawoptioninstruction();
+	context.fillStyle = colorset.fontcolor;
+	context.font = "50px Arial";
+	context.fillText("BACKGROUND COLOR:", canvas.width / 2 - 250, 100);
+	drawcurrencolorpalette(200, 200);
+}
+
+function changeBallColor(colorname)
+{
+	drawoptioninstruction();
+	context.fillStyle = colorset.fontcolor;
+	context.font = "50px Arial";
+	context.fillText("BALL COLOR:", canvas.width / 2 - 150, 100);
+	drawcurrencolorpalette(200, 200);
+	drawCircle(canvas.width / 2, canvas.height / 2, 50, colorset.ballcolor);
+}
+
+function changePlayer1Color(colorname)
+{
+	drawoptioninstruction();
+	context.fillStyle = colorset.fontcolor;
+	context.font = "50px Arial";
+	context.fillText("PLAYER 1 COLOR:", canvas.width / 2 - 200, 100);
+	drawcurrencolorpalette(200, 200);
+	drawRect(200, canvas.height / 2 + 20, 320, 30, colorset.player1color);
+}
+
+function changePlayer2Color(colorname)
+{
+	drawoptioninstruction();
+	context.fillStyle = colorset.fontcolor;
+	context.font = "50px Arial";
+	context.fillText("PLAYER 2 COLOR:", canvas.width / 2 - 200, 100);
+	drawcurrencolorpalette(200, 200);
+	drawRect(200, canvas.height / 2 + 20, 320, 30, colorset.player2color);
+}
+
+function changeNetColor(colorname)
+{
+	drawoptioninstruction();
+	context.fillStyle = colorset.fontcolor;
+	context.font = "50px Arial";
+	context.fillText("NET COLOR:", canvas.width / 2 - 150, 100);
+	drawcurrencolorpalette(200, 200);
+	drawNet();
+}
+
+function changeScoreColor(colorname)
+{
+	drawoptioninstruction();
+	context.fillStyle = colorset.fontcolor;
+	context.font = "50px Arial";
+	context.fillText("SCORE COLOR:", canvas.width / 2 - 180, 100);
+	drawcurrencolorpalette(200, 200);
+	drawScore();
+}
+
+
+function keyhookdownforgame(event)
 {
 	if (event.key === "ArrowUp" && player2.up !== 1)
 	{
@@ -80,9 +204,68 @@ document.addEventListener("keydown", (event) =>
 		player1.down = 1;
 		player1.dy += speed;
 	}
-});
+}
 
-document.addEventListener("keyup", (event) =>
+function keyhookdownforoption(event)
+{
+	keyoptionpressed = 0;
+}
+
+/* Mouvement des raquettes */
+document.addEventListener("keydown", (event) =>
+	{
+		if (start === 1)
+			keyhookdownforgame(event);
+		else if (option === 1)
+			keyhookdownforoption(event);
+	});
+	
+function keyhookupforoption(event)
+{
+	if (event.key === "Enter" && keyoptionpressed === 0)
+	{
+		if (optionindex === functionstochangeoption.length - 1)
+		{
+			option = 0;
+			optionindex = 0;
+			context.clearRect(0, 0, canvas.width, canvas.height);
+			context.fillStyle = colorset.backgroundcolor;
+			context.fillRect(0, 0, canvas.width, canvas.height);
+			wait();
+			return ;
+		}
+		optionindex++;
+		context.clearRect(0, 0, canvas.width, canvas.height);
+		context.fillStyle = colorset.backgroundcolor;
+		context.fillRect(0, 0, canvas.width, canvas.height);
+		functionstochangeoption[optionindex](colorset.fontcolor);
+	}
+	else if (event.key === "ArrowLeft" && keyoptionpressed === 0)
+	{
+		colorindex++;
+		if (colorindex >= colorpalette.length)
+			colorindex = 0;
+		changeColor();
+		context.clearRect(0, 0, canvas.width, canvas.height);
+		context.fillStyle = colorset.backgroundcolor;
+		context.fillRect(0, 0, canvas.width, canvas.height);
+		functionstochangeoption[optionindex](colorpalette[colorindex]);
+	}
+	else if (event.key === "ArrowRight" && keyoptionpressed === 0)
+	{
+		colorindex--;
+		if (colorindex < 0)
+			colorindex = colorpalette.length - 1;
+		changeColor();
+		context.clearRect(0, 0, canvas.width, canvas.height);
+		context.fillStyle = colorset.backgroundcolor;
+		context.fillRect(0, 0, canvas.width, canvas.height);
+		functionstochangeoption[optionindex](colorpalette[colorindex]);
+	}
+	keyoptionpressed = 1;
+}
+
+function keyhookupforgame(event)
 {
 	if (event.key === "ArrowUp" && player2.up === 1)
 	{
@@ -104,13 +287,22 @@ document.addEventListener("keyup", (event) =>
 		player1.down = 0;
 		player1.dy -= speed;
 	}
+}
+
+document.body.addEventListener("keyup", (event) =>
+{
+	if (start === 1)
+		keyhookupforgame(event);
+	else if (option === 1)
+		keyhookupforoption(event);
 });
 
 function playerMove()
 {
 	player1.y += player1.dy;
-
+	player2.y += player2.dy;
 	player1.y = Math.max(Math.min(player1.y, canvas.height - paddleHeight), 0);
+	player2.y = Math.max(Math.min(player2.y, canvas.height - paddleHeight), 0);
 }
 
 function ballMove()
@@ -148,7 +340,11 @@ function increaseScore()
 	if (score1 === 5 || score2 === 5)
 	{
 		context.clearRect(0, 0, canvas.width, canvas.height);
+		context.fillStyle = colorset.backgroundcolor;
+		context.fillRect(0, 0, canvas.width, canvas.height);
+		context.fillStyle = colorset.fontcolor;
 		context.fillText(score1 === 5 ? "Player 1 wins!" : "Player 2 wins!", canvas.width / 2 - 100, canvas.height / 2 + 25);
+		setTimeout(wait, 5000);
 		start = 0;
 		return 1;
 	}
@@ -181,13 +377,14 @@ function update()
 }
 
 /* Fonctions utilitaires */
-
 function countdown()
 {
 	let count = 3;
 	const interval = setInterval(() => {
 		context.clearRect(0, 0, canvas.width, canvas.height);
-		context.fillStyle = "#FFF";
+		context.fillStyle = colorset.backgroundcolor;
+		context.fillRect(0, 0, canvas.width, canvas.height);
+		context.fillStyle = colorset.fontcolor;
 		context.font = "100px Arial";
 		context.fillText(count, canvas.width / 2 - 25, canvas.height / 2 + 25);
 		count--;
@@ -195,6 +392,8 @@ function countdown()
 		{
 			clearInterval(interval);
 			context.clearRect(0, 0, canvas.width, canvas.height);
+			context.fillStyle = colorset.backgroundcolor;
+			context.fillRect(0, 0, canvas.width, canvas.height);
 			context.fillText("GO!", canvas.width / 2 - 75, canvas.height / 2 + 25);
 		}
 	}, 1000);
@@ -208,20 +407,58 @@ function loop()
 	requestAnimationFrame(loop);
 }
 
+function initvariables()
+{
+	score1 = 0;
+	score2 = 0;
+	player1.y = canvas.height / 2 - paddleHeight / 2;
+	player2.y = canvas.height / 2 - paddleHeight / 2;
+	ball.x = canvas.width / 2;
+	ball.y = canvas.height / 2;
+	ball.speed = 5;
+	ball.dx = 1;
+	ball.dy = Math.random() - 0.5;
+	player1.dy = 0;
+	player2.dy = 0;
+	player1.ismoving = false;
+	player2.ismoving = false;
+}
+
 function startGame()
 {
-	if (start === 1)
+	if (start === 1 || option === 1)
 		return ;
 	start = 1;
+	initvariables();
 	countdown();
 	setTimeout(loop, 5000);
 }
 
+function startOption()
+{
+	if (option === 1 || start === 1)
+		return ;
+	option = 1;
+	context.clearRect(0, 0, canvas.width, canvas.height);
+	context.fillStyle = colorset.backgroundcolor;
+	context.fillRect(0, 0, canvas.width, canvas.height);
+	functionstochangeoption[optionindex](colorset.fontcolor);
+}
+
 function wait()
 {
-	context.fillStyle = "#FFF";
+	if (start === 1 || option === 1)
+		return ;
+	context.clearRect(0, 0, canvas.width, canvas.height);
+	context.fillStyle = colorset.backgroundcolor;
+	context.fillRect(0, 0, canvas.width, canvas.height);
+	context.fillStyle = colorset.fontcolor;
 	context.font = "50px Arial";
 	context.fillText("Press red button to start", canvas.width / 2 - 250, canvas.height / 2);
+	context.font = "30px Arial";
+	context.fillText("Press blue button for options", canvas.width / 2 - 180, canvas.height - 20);
 }
+
+
 
 wait();
