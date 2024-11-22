2c2542a3b39611cd4cefaa774da7129c9b1d6337
ajasnin <arnaud.jasnin@gmail.com>
2024-11-22 15:15:13 +0100

    default/urls.py: add accounts

--- default/urls.py
+++ default/urls.py
@@ -20,6 +20,7 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('app.urls')),
+    path('accounts/', include('accounts.urls')),
     path('scores/', include('scores.urls')),
     path('ajastest/', include('ajastest.urls'))
 ]
