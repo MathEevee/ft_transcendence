82b325007cf9ddbd23578d18640b7837a0836cfc
ft_transcendence <ft_transcendence@transcendence.myguest.virtualbox.org>
2024-11-28 10:22:00 +0100

    up

--- app/static/chatbox.js
+++ app/static/chatbox.js
@@ -1,4 +1,11 @@
 document.addEventListener('DOMContentLoaded', () => {
+    for (let i = 0; tablist[i]; i++)
+    {
+        var newFriend = document.createElement("p"); //change to button to open chat with this user
+        newFriend.textContent = tablist[i];
+        newFriend.classList.add("friend");
+        document.getElementById('FriendList').appendChild(newFriend);
+    }
     liveChat();
 });
 
@@ -9,11 +16,13 @@ function liveChat(){
     const liveChat = document.getElementById('box');
     const minWin = document.getElementById('closeChat');
     const search = document.getElementById('addFriends');
+
+    search.value = "";
     
     function openChat(){
         ChatButton.style.display = "none";
         liveChat.style.display = "block";
-        //load all friends
+        //load all messages
     }
     
     function closeChat(){
@@ -28,16 +37,18 @@ function liveChat(){
 
 
 function addFriends(event){
+    var elemcontainer = document.getElementById('FriendList');
     if (event.key === 'Enter') {
         event.preventDefault();
         //add check is in database
         var inputValue = document.getElementById('addFriends').value;
         tablist.push(inputValue);
-        console.log(tablist);
         document.getElementById('addFriends').value = '';
         var newFriend = document.createElement("p"); //change to button to open chat with this user
         newFriend.classList.add("friend"); //add to the database
         newFriend.textContent = inputValue;
-        document.getElementById('FriendList').appendChild(newFriend);
+        elemcontainer.appendChild(newFriend);
+        // newFriend.scrollHeight = newFriend.height;
+        elemcontainer.scrollTop = elemcontainer.scrollHeight
     }
 }
