7addd8b4e2d265333512148fac82b3dd96bfe1d1
ft_transcendence <ft_transcendence@transcendence.myguest.virtualbox.org>
2024-11-30 11:44:57 +0100

    update chatbox

--- app/static/chatbox/chatbox.js
+++ app/static/chatbox/chatbox.js
@@ -1,69 +1,74 @@
-
-
 document.addEventListener('DOMContentLoaded', () => {
     liveChat();
 });
 
-var tablist = ["alice","bob","salut"]
+var tablist = ["alice", "bob", "salut"];
 
-function liveChat(){
+function liveChat() {
     const ChatButton = document.getElementById('chat');
     const liveChat = document.getElementById('box');
     const minWin = document.getElementById('closeChat');
     const search = document.getElementById('addFriends');
-    // const loadFriend = document.getElementById('friend');
-
-
-    for (let i = 0; tablist[i]; i++)
-    {
-        var newFriend = document.createElement("button"); //change to button to open chat with this user
+    const elemcontainer = document.getElementById('FriendList');
+    
+    
+    for (let i = 0; i < tablist.length; i++) {
+        var newFriend = document.createElement("button");
         newFriend.textContent = tablist[i];
         newFriend.classList.add("friend");
-        document.getElementById('FriendList').appendChild(newFriend);
+        elemcontainer.appendChild(newFriend);
+        
+        newFriend.addEventListener('click', function(event) {
+            loadBar(event);
+        });
     }
     
     search.value = "";
     
-    function openChat(){
-        var elemcontainer = document.getElementById('FriendList');
+    function openChat() {
         ChatButton.style.display = "none";
         liveChat.style.display = "block";
         elemcontainer.scrollTop = elemcontainer.scrollHeight;
-        var chat = document.createElement("text");
-        chat.textContent = "Bienvenue dans votre chat";
     }
     
-    function closeChat(){
+    function closeChat() {
         liveChat.style.display = "none";
         ChatButton.style.display = "flex";
     }
-
-    ChatButton.addEventListener('click', openChat)
-    minWin.addEventListener('click', closeChat)
-    // loadFriend.addEventListener('click', loadBar)
-    search.addEventListener('keydown', function(event) {addFriends(event)})
-};
-
-function loadBar(event){
-    var elem = document.getElementById('friend')
-    console.log(elem);
     
+    ChatButton.addEventListener('click', openChat);
+    minWin.addEventListener('click', closeChat);
+    
+    search.addEventListener('keydown', function(event) { 
+        addFriends(event); 
+    });
 }
 
+function loadBar(event) {
+    var barElement = document.getElementById('bar');
+    barElement.textContent = 'Private with : ';
+    var friendName = event.target.textContent;
+    barElement.textContent += friendName;
+    console.log(friendName);
+}
 
-function addFriends(event){
+function addFriends(event) {
     var elemcontainer = document.getElementById('FriendList');
     if (event.key === 'Enter') {
         event.preventDefault();
-        //add check is in database
         var inputValue = document.getElementById('addFriends').value;
         tablist.push(inputValue);
         document.getElementById('addFriends').value = '';
-        var newFriend = document.createElement("button"); //change to button to open chat with this user
-        newFriend.classList.add("friend"); //add to the database
+
+        var newFriend = document.createElement("button");
+        newFriend.classList.add("friend");
         newFriend.textContent = inputValue;
         elemcontainer.appendChild(newFriend);
-        // newFriend.scrollHeight = newFriend.height;
-        elemcontainer.scrollTop = elemcontainer.scrollHeight
+
+        newFriend.addEventListener('click', function(event) {
+            loadBar(event);
+        });
+
+        elemcontainer.scrollTop = elemcontainer.scrollHeight;
     }
 }
