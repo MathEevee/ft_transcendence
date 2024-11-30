adfedbc45d9acc9534e1914e9483ac732d32b7dc
ajasnin <arnaud.jasnin@gmail.com>
2024-11-30 17:23:23 +0100

    fix authe/

--- default/urls.py
+++ default/urls.py
@@ -4,7 +4,6 @@ from django.views.generic import RedirectView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-<<<<<<< HEAD
     # path('', include('app.urls')),
     # path('accounts/', include('accounts.urls')),
     # path('scores/', include('scores.urls')),
@@ -14,9 +13,4 @@ urlpatterns = [
     path('game/', include('apps.game.urls')),
     # Redirect root URL to the home app
     path('', RedirectView.as_view(url='/home/', permanent=False)),
-=======
-    path('', include('app.urls')),
-    path('accounts/', include('accounts.urls')),
-    path('scores/', include('scores.urls')),
->>>>>>> main
 ]
