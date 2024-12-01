b0ca14cbdd65e59e11221ca50da202882e2312d9
ajasnin <arnaud.jasnin@gmail.com>
2024-12-01 18:10:28 +0100

    apps/authe/admin.py : add item profil_picture

--- apps/authe/admin.py
+++ apps/authe/admin.py
@@ -5,7 +5,7 @@ from .models import CustomUser
 @admin.register(CustomUser)
 class CustomUserAdmin(UserAdmin):
     model = CustomUser
-    list_display = ['username', 'email', 'intra_id', 'is_staff', 'is_superuser']
+    list_display = ['username', 'email', 'intra_id', 'is_staff', 'is_superuser', 'profil_picture']
     list_filter = ['is_staff', 'is_superuser']
     fieldsets = UserAdmin.fieldsets + (
         (None, {'fields': ('intra_id', 'profil_picture')}),
