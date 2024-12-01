578ded237d8ce826a2cde608d506e8d32bb3e046
ajasnin <arnaud.jasnin@gmail.com>
2024-12-01 16:53:54 +0100

    modif settings & urls after merge

--- default/urls.py
+++ default/urls.py
@@ -4,9 +4,6 @@ from django.views.generic import RedirectView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    # path('', include('app.urls')),
-    # path('accounts/', include('accounts.urls')),
-    # path('scores/', include('scores.urls')),
     path('authe/', include('apps.authe.urls')),
     path('', include('apps.home.urls')),
     path('profil/', include('apps.profil.urls')),
