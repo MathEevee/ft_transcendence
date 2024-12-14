8c6340603784ee1503187d52142865fa38e20f2c
ajasnin <arnaud.jasnin@gmail.com>
2024-12-14 10:47:40 +0100

    create apps.authe.context_processors.py & add button 'Admin' into navbar

--- default/settings.py
+++ default/settings.py
@@ -76,6 +76,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'apps.authe.context_processors.staff_status',
             ],
         },
     },
