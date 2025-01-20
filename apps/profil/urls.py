e4ae2eb19476487325c3d83dd86a0c9fcf4ef696
PageBlanche <axeldubois@orange.fr>
2025-01-20 13:50:29 +0100

    del on line in profil/url

--- apps/profil/urls.py
+++ apps/profil/urls.py
@@ -5,6 +5,6 @@ app_name = 'profil'
 
 urlpatterns = [
     path('', views.profil_view, name='profil'),
-    path('logout/', views.logout_view, name='logout'),
     path('account/<int:id>/', views.get_profil_view, name='account'),
+    path('<str:username>/', views.profil_view, name='profil'),
 ]
