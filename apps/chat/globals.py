e136ac2ac58bf9346d55cfcda8da206403c25f49
Transcendence <transcendence@Transcendence.myguest.virtualbox.org>
2025-01-22 14:54:24 +0100

    mini patch chatbox

--- apps/chat/globals.py
+++ apps/chat/globals.py
@@ -1,7 +1,7 @@
 from collections import defaultdict, deque
 
 
-user_sockets = {} # todo AXEL: au lieu d'enregistrer un websocket par username, enregister une liste de websocket par username
+user_sockets = {}
+# user_sockets[username] = [...userWebsockets]
 conversations = defaultdict(lambda: deque(maxlen=50))
-online_users = set()
 conv_rooms = defaultdict(set)
