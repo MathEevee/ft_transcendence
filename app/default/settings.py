d29546b62f76cdc0f0b88878d59a7b29cb5925b1
ajasnin <arnaud.jasnin@gmail.com>
2025-01-26 14:23:11 +0100

    add ip_VM into settings.py

--- app/default/settings.py
+++ app/default/settings.py
@@ -26,11 +26,13 @@ DEBUG = True
 CSRF_TRUSTED_ORIGINS = [
     "http://localhost:8080",
     "http://127.0.0.1:8080",
+    "http://10.13.248.126:8080",
 ]
 
 ALLOWED_HOSTS = [
     "localhost",
     "127.0.0.1",
+    "10.13.248.126",
 ]
 
 # Application definition
